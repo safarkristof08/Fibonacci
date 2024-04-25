acab25d2a384db8f7b361843ee5ea7a089602bd0
Kristóf Bence Sáfár <safarkristof08@gmail.com>
2024-04-25 12:55:27 +0200

    SQL adatbázishoz csatlakozás

---
+++ gyak09/Form1.cs
@@ -0,0 +1,32 @@
+using gyak09.Models;
+
+namespace gyak09
+{
+    public partial class Form1 : Form
+    {
+
+        Models.StudentContext studentContext = new Models.StudentContext();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            studentBindingSource.DataSource = studentContext.Students.ToList(); ;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                studentContext.SaveChanges();
+            }
+            catch (Exception kivétel)
+            {
+                MessageBox.Show(kivétel.InnerException.Message);
+            }
+        }
+    }
+}
